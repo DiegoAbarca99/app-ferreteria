5c8aa8970b1e393c20c85b276b35d4efa519b5b9
DiegoAbarcaRodriguez <abarcarodriguezdiego@gmail.com>
2023-03-23 23:53:22 -0600

    Solucion problema de rutas get  con query parameters en producciòn

--- Router.php
+++ Router.php
@@ -20,7 +20,12 @@ class Router
     public function comprobarRutas()
     {
 
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'];
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
+
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
@@ -30,7 +35,7 @@ class Router
         }
 
 
-        if ( $fn ) {
+        if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
