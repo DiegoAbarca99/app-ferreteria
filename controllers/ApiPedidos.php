be42b965255bc8a34f15a252b7fb6b08ca670fa4
DiegoAbarcaRodriguez <abarcarodriguezdiego@gmail.com>
2023-03-24 23:57:57 -0600

    Prueba 18  correción con api levantar pedido

--- controllers/ApiPedidos.php
+++ controllers/ApiPedidos.php
@@ -20,10 +20,7 @@ class ApiPedidos
             isAuth();
             isProveedor();
 
-            header('Access-Control-Allow-Origin: *');
-            header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-            header('content-type: application/json; charset=utf-8');
+    
 
 
             if (empty($_POST['abono']))
