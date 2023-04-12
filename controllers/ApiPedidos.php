0b7c558d8d97a3fae9382f532e9372b53bf8b87b
DiegoAbarcaRodriguez <abarcarodriguezdiego@gmail.com>
2023-04-11 23:53:36 -0500

    Modificaciones realizadas para el deployment

--- controllers/ApiPedidos.php
+++ controllers/ApiPedidos.php
@@ -20,14 +20,6 @@ class ApiPedidos
             isAuth();
             isProveedor();
 
-            header('Access-Control-Allow-Origin: *');
-            header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-            header('content-type: application/json; charset=utf-8');
-
-            echo('hola mundo');
-
-           
 
 
             if (empty($_POST['abono']))
