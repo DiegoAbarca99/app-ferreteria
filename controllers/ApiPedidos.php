8b5641abc7f51ec1d6e6522ebf7bb4230a0141ce
DiegoAbarcaRodriguez <abarcarodriguezdiego@gmail.com>
2023-05-02 17:15:42 -0500

    Correciòn CORS PHP

--- controllers/ApiPedidos.php
+++ controllers/ApiPedidos.php
@@ -23,13 +23,6 @@ class ApiPedidos
             isProveedor();
 
 
-            header('Access-Control-Allow-Origin: *');
-            header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-
-
-
-
 
             if (empty($_POST['abono']))
                 $_POST['abono'] = '0';
--- includes/app.php
+++ includes/app.php
@@ -2,7 +2,12 @@
 
 use Dotenv\Dotenv;
 use Model\ActiveRecord;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 // Añadir Dotenv
 $dotenv = Dotenv::createImmutable(__DIR__);
